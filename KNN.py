4851df41486612004d30c46128f02f124de988c9
cotrang20012 <54300415+cotrang20012@users.noreply.github.com>
2022-12-05 17:50:46 +0700

    update hoiquydathuc

--- KNN.py
+++ KNN.py
@@ -8,8 +8,11 @@ import joblib
 import cv2
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
-st.set_option('deprecation.showPyplotGlobalUse', False)
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 #GiamDanDaoHam
 #bai01
@@ -85,6 +88,102 @@ def mainGDDH03():
 st.subheader('bai03 ')
 mainGDDH03()
 ######################################################
+
+#HoiQuyDaThuc
+N = 30
+N_test = 30
+#bai01
+def mainHQDT01():
+    np.random.seed(100)
+    X = np.random.rand(N,1)*5
+    y = 3*(X-2) * (X-3)*(X-4) + 10*np.random.randn(N,1)
+
+    X_true = np.linspace(0,5,51)
+    y_true = 3*(X_true-2) * (X_true-3)*(X_true-4)
+
+    X_test = (np.random.randn(N_test,1) - 1/8)*10
+    y_test = 3*(X_test -2)*(X_test-3)*(X_test-4)+10*np.random.randn(N,1)
+
+
+    plt.plot(X,y,'ro',markersize=3)
+    plt.plot(X_true,y_true,'y') 
+    
+    poly_features = PolynomialFeatures(degree=2, include_bias=False)
+    X_poly = poly_features.fit_transform(X)
+    # for i in range(N):
+    #     print('%10.4f, %10.4f %10.4f'%(X[i,0],X_poly[i,0],X_poly[i,1]))
+    lin_reg = LinearRegression()
+    lin_reg.fit(X_poly, y)
+    print(lin_reg.intercept_, lin_reg.coef_)
+    w0 = lin_reg.intercept_[0]
+    w1 = lin_reg.coef_[0,0]
+    w2 = lin_reg.coef_[0,1]
+
+    y_predict = w0 + w1*X_true + w2*X_true**2
+
+    plt.plot(X_true,y_predict,'r')
+    #tính sai số trên tập test
+    X_test_poly = poly_features.fit_transform(X_test)
+    y_test_predict = lin_reg.predict(X_test_poly)
+    mse_test = mean_squared_error(y_test,y_test_predict)
+    rmse_test = np.sqrt(mse_test)
+    print('Sai số bình phương trung bình - test:')
+    print('%.4f' % rmse_test)
+    fig = plt.show()
+    st.pyplot(fig)
+    textHQDT01 = 'Sai số bình phương trung bình test: ' + '%.4f' % rmse_test
+    st.subheader(textHQDT01)
+
+st.title('Hồi quy đa thức ')
+st.subheader('bai01')
+mainHQDT01()
+#bai02
+def mainHQDT02(): 
+    np.random.seed(100)
+    X = np.random.rand(N,1)*5
+    y = 3*(X-2) * (X-3)*(X-4) + 10*np.random.randn(N,1)
+
+    X_true = np.linspace(0,5,51)
+    y_true = 3*(X_true-2) * (X_true-3)*(X_true-4)
+
+    X_test = (np.random.randn(N_test,1) - 1/8)*10
+    y_test = 3*(X_test -2)*(X_test-3)*(X_test-4)+10*np.random.randn(N,1)
+
+
+    plt.plot(X,y,'ro',markersize=3)
+    plt.plot(X_true,y_true,'y')
+    
+    poly_features = PolynomialFeatures(degree=4, include_bias=False)
+    X_poly = poly_features.fit_transform(X)
+    # for i in range(N):
+    #     print('%10.4f, %10.4f %10.4f'%(X[i,0],X_poly[i,0],X_poly[i,1]))
+    lin_reg = LinearRegression()
+    lin_reg.fit(X_poly, y)
+    print(lin_reg.intercept_, lin_reg.coef_)
+    w0 = lin_reg.intercept_[0]
+    w1 = lin_reg.coef_[0,0]
+    w2 = lin_reg.coef_[0,1]
+    w3 = lin_reg.coef_[0,2]
+    w4 = lin_reg.coef_[0,3]
+
+
+    y_predict = w0 + w1*X_true + w2*X_true**2 +w3*X_true**3 + w4*X_true**4
+
+    plt.plot(X_true,y_predict,'r')
+    #tính sai số trên tập test
+    X_test_poly = poly_features.fit_transform(X_test)
+    y_test_predict = lin_reg.predict(X_test_poly)
+    mse_test = mean_squared_error(y_test,y_test_predict)
+    rmse_test = np.sqrt(mse_test)
+    print('Sai số bình phương trung bình - test:')
+    print('%.4f' % rmse_test)
+    fig = plt.show()
+    st.pyplot(fig)
+    textHQDT02 = 'Sai số bình phương trung bình test: ' + '%.4f' % rmse_test
+    st.subheader(textHQDT02)
+
+st.subheader('bai02')
+mainHQDT02()
 
 #KNN
 #knn = joblib.load("D:\HT\hk7\ML\KNN\knn_digit.pkl")
